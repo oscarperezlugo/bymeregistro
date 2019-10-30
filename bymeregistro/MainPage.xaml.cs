fae6f24efa306b09da9e3d49ef227ed7e4506447
oscarperezlugo <operezlugoes@gmail.com>
2019-10-30 09:51:32 +0100

    App
    App

---
+++ bymeregistro/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace bymeregistro
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public async void Register_Clicked(object sender, EventArgs e)
+        {
+            RegisterPage myHomePage = new RegisterPage();
+            NavigationPage.SetHasNavigationBar(myHomePage, true);
+            await Navigation.PushModalAsync(myHomePage);
+        }
+        public async void Date_Clicked(object sender, EventArgs e)
+        {
+            DatePage myHomePage = new DatePage();
+            NavigationPage.SetHasNavigationBar(myHomePage, true);
+            await Navigation.PushModalAsync(myHomePage);
+        }
+        public async void Servicio_Clicked(object sender, EventArgs e)
+        {
+            ServicioPage myHomePage = new ServicioPage();
+            NavigationPage.SetHasNavigationBar(myHomePage, true);
+            await Navigation.PushModalAsync(myHomePage);
+        }
+        public async void Main_Clicked(object sender, EventArgs e)
+        {
+            MainPage myHomePage = new MainPage();
+            NavigationPage.SetHasNavigationBar(myHomePage, true);
+            await Navigation.PushModalAsync(myHomePage);
+        }
+        public async void Perfil_Clicked(object sender, EventArgs e)
+        {
+            PerfilPage myHomePage = new PerfilPage();
+            NavigationPage.SetHasNavigationBar(myHomePage, true);
+            await Navigation.PushModalAsync(myHomePage);
+        }
+
+    }
+}
---
+++ bymeregistro/PerfilPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace bymeregistro
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PerfilPage : ContentPage
+    {
+        public PerfilPage()
+        {
+            InitializeComponent();
+        }
+        public async void Register_Clicked(object sender, EventArgs e)
+        {
+            RegisterPage myHomePage = new RegisterPage();
+            NavigationPage.SetHasNavigationBar(myHomePage, true);
+            await Navigation.PushModalAsync(myHomePage);
+        }
+        public async void Date_Clicked(object sender, EventArgs e)
+        {
+            DatePage myHomePage = new DatePage();
+            NavigationPage.SetHasNavigationBar(myHomePage, true);
+            await Navigation.PushModalAsync(myHomePage);
+        }
+        public async void Servicio_Clicked(object sender, EventArgs e)
+        {
+            ServicioPage myHomePage = new ServicioPage();
+            NavigationPage.SetHasNavigationBar(myHomePage, true);
+            await Navigation.PushModalAsync(myHomePage);
+        }
+        public async void Main_Clicked(object sender, EventArgs e)
+        {
+            MainPage myHomePage = new MainPage();
+            NavigationPage.SetHasNavigationBar(myHomePage, true);
+            await Navigation.PushModalAsync(myHomePage);
+        }
+        public async void Perfil_Clicked(object sender, EventArgs e)
+        {
+            PerfilPage myHomePage = new PerfilPage();
+            NavigationPage.SetHasNavigationBar(myHomePage, true);
+            await Navigation.PushModalAsync(myHomePage);
+        }
+    }
+}
